3c4e429c952ef7e4b2a6e630383e068f7074f7b6
NathanKell <nathankell@gmail.com>
2021-05-11 20:14:06 -0700

    Add a version of ModuleAnimateGeneric (ModuleAnimateGenericExtra) that allows changing the name of the DeployLimit slider, and hiding the Toggle event.

---
+++ Source/ModuleAnimateGenericExtra.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Linq;
+
+namespace StagedAnimation
+{
+    public class ModuleAnimateGenericExtra : ModuleAnimateGeneric, IScalarModule
+    {
+        [KSPField]
+        public string deployLimitName = "";
+
+        [KSPField]
+        public bool showToggle = true;
+
+        protected BaseEvent toggleEvt;
+        protected BaseAction toggleAct;
+
+        public override void OnAwake()
+        {
+            base.OnAwake();
+
+            toggleEvt = Events["Toggle"];
+            toggleAct = Actions["ToggleAction"];
+        }
+
+        public override void OnStart(PartModule.StartState state)
+        {
+            base.OnStart(state);
+
+            if (!string.IsNullOrEmpty(deployLimitName))
+            {
+                Fields["deployPercent"].guiName = deployLimitName;
+            }
+
+            if (!showToggle)
+            {
+                toggleEvt.guiActiveEditor = false;
+                toggleEvt.guiActive = false;
+                toggleEvt.guiActiveUnfocused = false;
+                toggleEvt.guiActiveUncommand = false;
+            }
+        }
+
+        public new void SetUIWrite(bool state)
+        {
+            toggleEvt.guiActive = state && allowManualControl && showToggle;
+            toggleEvt.guiActiveUnfocused = state && allowManualControl && showToggle;
+        }
+    }
+}
